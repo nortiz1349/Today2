4da14b215ca711c1b1670b0aac66981c177e2bf3
Nortiz <nortiz1349@gmail.com>
2022-09-21 11:23:20 +0900

    Initial Commit

---
+++ Today2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Today2
+//
+//  Created by Nortiz M1 on 2022/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
